62b01227e18ef4398c0b51ff2d1324cca1a014d8
silenthill2024 <carosalasjoseramon@gmail.com>
2026-09-22 17:34:18 -0600

    icono cambiado de nuevo2

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,8 @@ const FILES_TO_CACHE = [
     "./manifest.json",
     "./css/styles.css",
     "./js/app.js",
-    "./icons/Caro192x192.png",
-    "./icons/Caro512x512.png"
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
 ];
 
 
